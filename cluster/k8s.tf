aa36273a036b88684e09a7b91e9e9815b3bc6713
Patrick Losso <46010586+plosso@users.noreply.github.com>
2023-05-07 10:55:18 -0400

    Remove unneeded k8s role

--- cluster/k8s.tf
+++ cluster/k8s.tf
@@ -32,32 +32,3 @@ resource "kubernetes_service_account" "jenkins_admin_user" {
     name = "${kubernetes_secret.jenkins_admin_password.metadata[0].name}"
   }
 }
-
-# resource "kubernetes_role" "jenkins_role" {
-#   metadata {
-#     name      = "jenkins_role"
-#   }
-
-#   rule {
-#     api_groups = [""]
-#     resources  = ["secrets"]
-#     verbs      = ["get", "watch", "list"]
-#   }
-# }
-
-# resource "kubernetes_role_binding" "jenkins_role_binding" {
-#   metadata {
-#     name      = "jenkins_role_binding"
-#   }
-#   role_ref {
-#     api_group = "rbac.authorization.k8s.io"
-#     kind      = "Role"
-#     name      = "jenkins"
-#   }
-
-#   subject {
-#     kind      = "ServiceAccount"
-#     name      = "jenkins"
-#     namespace = "devops-tools"
-#   }
-# }
